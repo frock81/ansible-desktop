43281a843749281783f737e7914e9cb8db86c345
Fabio Rocha <frock81@yahoo.com.br>
2018-05-25 18:45:50 -0300

    Role frock81.keyboard

--- create_role.sh
+++ create_role.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
 ROLE_NAME="$1"
+ROLE_DIR=./roles/$ROlE_NAME
 
-mkdir -p ./roles/$ROLE_NAME/tasks
-mkdir -p ./roles/$ROLE_NAME/defaults
-printf -- "---\n\n" > ./roles/$ROLE_NAME/tasks/main.yml 
-printf -- "---\n\n" > ./roles/$ROLE_NAME/defaults/main.yml 
+create_dirs()
+{
+  mkdir -p $ROLE_DIR/tasks
+  mkdir -p $ROLE_DIR/defaults
+}
+
+create_files()
+{
+  for directory in tasks defaults; do
+    if [ ! -f "$ROLE_DIR/$directory/main.yml"]; then
+      printf -- "--\n\n" > $ROLE_DIR/$directory/main.yml
+    fi
+  done
+}
+
+main()
+{
+  create_dirs
+  create_files
+}
+
+main
--- roles/frock81.gitconfig/README.md
+++ roles/frock81.gitconfig/README.md
@@ -2,4 +2,10 @@
 
 ## Default Variables
 
-
+gitconfig_items:
+  - name: user.name
+    value: Fabio Rocha
+  - name: user.email
+    value: frock81@yahoo.com.br
+  - name: core.editor
+    value: vim
